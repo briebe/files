aeccf5f98135f51e14f6ddf7c3f396b38b76acfd
Lena <annalena.stohwasser@hotmail.de>
2017-11-02 12:40:45 +0100

    Update assign1.cpp
    Put input Algo and Dijsktra Algo together.

--- assign1.cpp
+++ assign1.cpp
@@ -1,51 +1,82 @@
-#include<iostream>
-#include<queue>
-#include<list>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+
+#define INF 0x3f3f3f3f
 
 using namespace std;
-#define INF 0x3f3f3f3f
- 
+
 typedef pair<int, int> iPair;
- 
-// Graph using adjacency list representation
-class Graph
-{
-    int V;    // No. of vertices
- 
-    list< pair<int, int> > *adj;
- 
-public:
-    Graph(int V);  // Constructor
- 
-    // function to add an edge to graph
-    void addEdge(int u, int v, int w);
- 
-    void dijkstra(int s);
-};
- 
-Graph::Graph(int V)
-{
-    this->V = V;
-    adj = new list<iPair> [V];
-}
- 
-void Graph::addEdge(int u, int v, int w)
-{
-    adj[u].push_back(make_pair(v, w));
-    adj[v].push_back(make_pair(u, w));
-}
- 
-void Graph::dijkstra(int src)
-{
+
+int main() {
+	ifstream data;
+	data.open("dat2.txt",ios_base::in);
+	
+	string line;
+	int counter = 0;
+	
+	// Jumping over the node names til #
+	
+	while(getline(data,line)){
+		
+		//cout << line << "\n";
+		
+		if(line != "#"){
+					
+		counter++;
+		}
+		
+		if(line == "#"){
+			break;
+		}
+	}
+	
+		float datamatrix[counter][counter] = {0};
+
+	//putting node numbers and values into a matrix
+
+		while(getline(data,line)){
+			
+			
+			string space = " ";
+			
+			int found = line.find(space);
+			int found2 = line.find(space, found+1);
+			
+			int node1 = stoi(line.substr(0, found));
+			int node2 = stoi(line.substr(found+1, found2-found-1));
+			float weight = stof(line.substr(found2+1));
+		
+			
+			//cout << node1 << " " << node2 << endl;
+			//cout << weight << endl;
+			
+			
+			datamatrix[node1][node2] = weight;
+			
+		};
+	
+	// Matrix to file(so i could open it with excel and see it)		
+		ofstream output("output.txt");
+		for (int i=0;i<counter;i++){
+			for(int j=0;j<counter;j++){
+				output << datamatrix[i][j] << " ";
+			}
+			output << "\n";
+		}
+		
+
 	// Creates a Min heap
-    // see http://geeksquiz.com/implement-min-heap-using-stl/ for more explanations
     priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
 
 
 	// Initiate start parameters
 
 	// Creates an vector for all distances and fill it with infinite
-    vector<int> distances(V, INF);
+    vector<int> distances(counter, INF);
+
+	int src = 0;
 	// Push startVertex into priority queue and set it's distance to 0
     pq.push(make_pair(0, src));
     distances[src] = 0;
@@ -56,51 +87,29 @@ void Graph::dijkstra(int src)
 	      int selectedVertex = pq.top().second;
 		  pq.pop();
 
-		  list< pair<int, int> >::iterator checkVertex;
-		  checkVertex = adj[selectedVertex].begin();
-
-		  for(checkVertex; checkVertex != adj[selectedVertex].end(); ++checkVertex)
+		  for(int i = 0; i < counter; ++i)
 		  {
-			  int checkVertexWeight = (*checkVertex).second;
-			  int checkVertexLabel = (*checkVertex).first;
-
-			if (distances[checkVertexLabel] > distances[selectedVertex] + checkVertexWeight)
+			  if(datamatrix[selectedVertex][i] != 0 ){
+			  int weightOfVertexToCheck = datamatrix[selectedVertex][i];
+			  int labelOfVertexToCheck = i;
+			   //printf("selectedVertex: %d , labelOfVertexToCheck: %d\n",selectedVertex, labelOfVertexToCheck);
+			   //printf("distances[labelOfVertexToCheck]: %d > distances[selectedVertex]: %d + weightOfVertexToCheck: %d\n",distances[labelOfVertexToCheck], distances[selectedVertex], weightOfVertexToCheck);
+			if (distances[labelOfVertexToCheck] > distances[selectedVertex] + weightOfVertexToCheck)
             {
+				//printf("\nUpdate\n\n");
                 // Updating distance
-                distances[checkVertexLabel] = distances[selectedVertex] + checkVertexWeight;
-                pq.push(make_pair(distances[checkVertexLabel], checkVertexLabel));
+                distances[labelOfVertexToCheck] = distances[selectedVertex] + weightOfVertexToCheck;
+                pq.push(make_pair(distances[labelOfVertexToCheck], labelOfVertexToCheck));
             }
-
+			}
 		  }
 
     }
  
     // Print shortest distances
-    printf("Vertex   Distance from Source\n");
-    for (int i = 0; i < V; ++i)
-        printf("%d \t\t %d\n", i, distances[i]);
-}
- 
-int main()
-{
-    // create graph
-    int V = 6;
-    Graph g(V);
+    printf("Vertex || Distance from source\n");
+    for (int i = 0; i < counter; ++i)
+        printf("%d || %d\n", i, distances[i]);
 
-	//example graph here: https://www.youtube.com/watch?v=S8y-Sk7u1So
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 2, 2);
-    g.addEdge(0, 4, 4);
-    g.addEdge(1, 2, 1);
-    g.addEdge(2, 3, 3);
-    g.addEdge(2, 4, 5);
-    g.addEdge(3, 5, 2);
-    g.addEdge(4, 5, 1);
-    
-    g.dijkstra(0);
-    
-    system("PAUSE");
- 
-    return 0;
-}
-
+return 0;
+ }
